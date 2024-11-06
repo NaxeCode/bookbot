b58cd4b6d9fd82a9ce0e93ba57aca5b0e41a5c49
“NaxeCode” <15963333+NaxeCode@users.noreply.github.com>
2024-11-06 15:05:26 -0500

    add main function to read files, also added wordcount function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(countWords(file_contents))
+
+
+def countWords(words):
+    wordCount = 0
+    for word in words:
+        wordCount += 1
+    return wordCount
+
+main()
