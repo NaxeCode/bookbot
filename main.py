d3e49445a71b4fdc3612dc22e7ea2b4a430a5448
“NaxeCode” <15963333+NaxeCode@users.noreply.github.com>
2024-11-10 12:01:42 -0500

    added count characters function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(countWords(file_contents))
+        #print(countWords(file_contents))
+        print(countCharacters(file_contents))
 
 
 def countWords(words):
@@ -10,5 +11,17 @@ def countWords(words):
     for word in txt:
         wordCount += 1
     return wordCount
+
+def countCharacters(book):
+    dict = {}
+    for char in book:
+        if char.isalpha():
+
+            if char in dict:
+                dict[char] += 1
+            else:
+                dict[char.lower()] = 1
+    print(dict)
+    return type(dict)
 
 main()
