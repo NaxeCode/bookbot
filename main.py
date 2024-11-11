44d3013b9eb797a9eb5a329d7a32a62df2a11a7e
“NaxeCode” <15963333+NaxeCode@users.noreply.github.com>
2024-11-11 14:42:45 -0500

    add report printing functionality

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        #print(countWords(file_contents))
-        print(countCharacters(file_contents))
+    book = "books/frankenstein.txt"
+    path = "./" + book
 
+    with open(path) as f:
+        file_contents = f.read()
+        print(f"--- Begin report of {book} ---\n")
+        countWords(file_contents)
+        countCharacters(file_contents)
+        print("--- End of report ---")
+        
 
 def countWords(words):
     wordCount = 0
     txt = words.split()
     for word in txt:
         wordCount += 1
-    return wordCount
+    print(f"{wordCount} words found in the book!\n")
 
 def countCharacters(book):
     dict = {}
@@ -21,7 +26,7 @@ def countCharacters(book):
                 dict[char] += 1
             else:
                 dict[char.lower()] = 1
-    print(dict)
-    return type(dict)
+    for char in dict:
+        print(f"the character '{char}' has been found {dict[char]} times!")
 
 main()
